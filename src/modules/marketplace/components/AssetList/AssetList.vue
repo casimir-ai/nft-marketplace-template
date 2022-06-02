d45a4915ea2f369f3fe5ec98d5135a1cd0b3f26d
Stacey Korbit <nastyasworld@gmail.com>
2022-06-02 21:58:42 +0300

    fix: fix nft list

--- src/modules/marketplace/components/AssetList/AssetList.vue
+++ src/modules/marketplace/components/AssetList/AssetList.vue
@@ -9,28 +9,30 @@
             v-model="sort"
             :label="$t('marketplace.assetList.sorting')"
             :items="sortingOptions"
-            @change="handleSelectChange"
           />
         </template>
       </vex-section-title>
 
-      <v-row>
-        <v-col
-          v-for="asset in assetList"
-          :key="asset._id"
-          cols="12"
-          md="6"
-          lg="3"
-        >
-          <router-link
-            :to="{name: 'assetDetails', params: {id: asset._id}}"
-            class="text-decoration-none"
-          >
-            <asset-card :content="asset" />
-          </router-link>
-        </v-col>
-      </v-row>
-      <infinite-loading v-if="assetList.length" @infinite="loadMoreAssets" />
+      <assets-infinite-scroll :sort="sort">
+        <template #default="{ assets }">
+          <v-row>
+            <v-col
+              v-for="asset in assets"
+              :key="asset._id"
+              cols="12"
+              md="6"
+              lg="3"
+            >
+              <router-link
+                :to="{name: 'assetDetails', params: {id: asset._id}}"
+                class="text-decoration-none"
+              >
+                <asset-card :content="asset" />
+              </router-link>
+            </v-col>
+          </v-row>
+        </template>
+      </assets-infinite-scroll>
     </ve-stack>
   </vex-section>
 </template>
@@ -39,9 +41,7 @@
   import { VexSection, VexSectionTitle } from '@deip/vuetify-extended';
   import { VeStack } from '@deip/vue-elements';
   import { AssetCard } from '@/components';
-  import InfiniteLoading from 'vue-infinite-loading';
-
-  const PAGE_SIZE = 10;
+  import { AssetsInfiniteScroll } from '@/components/AssetsInfiniteScroll';
 
   export default {
     name: 'AssetList',
@@ -51,14 +51,12 @@
       VexSection,
       VexSectionTitle,
       VeStack,
-      InfiniteLoading
+      AssetsInfiniteScroll
     },
 
     data() {
       return {
         loading: false,
-        currentPage: 0,
-        assetList: [],
         sort: {},
         sortingOptions: [
           {
@@ -91,43 +89,7 @@
           }
         ]
       };
-    },
-
-    created() {
-      this.getList(0);
-    },
-
-    methods: {
-      async getList(page) {
-        const query = {
-          page,
-          pageSize: PAGE_SIZE,
-          sort: this.sort
-        };
-        try {
-          this.loading = true;
-          const { items } = await this.$store.dispatch('projectContent/getListPaginated', query);
-          this.assetList = this.assetList.concat(items);
-          const authorIds = this.assetList.map((asset) => asset.authors[0]);
-          const uniqueAuthorIds = Array.from(new Set(authorIds));
-          await this.$store.dispatch('users/getList', uniqueAuthorIds);
-          this.loading = false;
-        } catch (error) {
-          console.error(error);
-        }
-      },
-
-      async loadMoreAssets($state) {
-        this.currentPage++;
-        await this.getList(this.currentPage);
-        $state.loaded();
-      },
-
-      handleSelectChange() {
-        this.currentPage = 0;
-        this.assetList = [];
-        this.getList(0);
-      }
     }
+
   };
 </script>
