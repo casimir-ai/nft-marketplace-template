73cb5b30ada8c988b57cfaa8b01e738bb4481407
Stacey Korbit <nastyasworld@gmail.com>
2022-07-07 20:05:22 +0300

    fix: CAS-119 add back button to attributes and layouts pages

--- src/modules/admin/components/attributes/AdminAttributesForm/AdminAttributesForm.vue
+++ src/modules/admin/components/attributes/AdminAttributesForm/AdminAttributesForm.vue
@@ -1,17 +1,28 @@
 <template>
-  <vex-section max-width="800" class="mx-auto pa-0">
-    <ve-stack :gap="32">
-      <vex-section-title :title="title" />
+  <v-sheet>
+    <v-btn
+      class="mt-2 ml-2"
+      icon
+      style="pointer-events: all"
+      @click="$router.back()"
+    >
+      <v-icon>mdi-arrow-left</v-icon>
+    </v-btn>
 
-      <attribute-edit
-        v-if="ready"
-        :mode="mode"
-        :value="attributeData"
-        @success="onSuccess"
-        @error="onError"
-      />
-    </ve-stack>
-  </vex-section>
+    <vex-section max-width="800" class="mx-auto pa-0">
+      <ve-stack :gap="32">
+        <vex-section-title :title="title" />
+
+        <attribute-edit
+          v-if="ready"
+          :mode="mode"
+          :value="attributeData"
+          @success="onSuccess"
+          @error="onError"
+        />
+      </ve-stack>
+    </vex-section>
+  </v-sheet>
 </template>
 
 <script>
--- src/modules/admin/components/attributes/AdminAttributesSettings/AdminAttributesSettings.vue
+++ src/modules/admin/components/attributes/AdminAttributesSettings/AdminAttributesSettings.vue
@@ -1,10 +1,21 @@
 <template>
-  <vex-section max-width="1200" class="mx-auto pa-0">
-    <ve-stack :gap="32">
-      <vex-section-title title="Attribute settings" />
-      <attributes-setup :value="$store.getters['attributes/settings']" />
-    </ve-stack>
-  </vex-section>
+  <v-sheet>
+    <v-btn
+      class="mt-2 ml-2"
+      icon
+      style="pointer-events: all"
+      @click="$router.back()"
+    >
+      <v-icon>mdi-arrow-left</v-icon>
+    </v-btn>
+
+    <vex-section max-width="1200" class="mx-auto pa-0">
+      <ve-stack :gap="32">
+        <vex-section-title title="Attribute settings" />
+        <attributes-setup :value="$store.getters['attributes/settings']" />
+      </ve-stack>
+    </vex-section>
+  </v-sheet>
 </template>
 
 <script>
